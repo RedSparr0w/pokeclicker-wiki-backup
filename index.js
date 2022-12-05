da0280073979d28fef144d803d39719941dcf19d
Danial <admin@redsparr0w.com>
2022-12-05 21:25:26 +1300

    s

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-const fs = require('fs')
-const XmlSplit = require('xmlsplit')
+(async () => {
+  const fs = require('fs')
+  const XmlSplit = require('xmlsplit')
 
-fs.mkdir('data', { recursive: true }, console.error);
-fs.mkdir('data-dump', { recursive: true }, console.error);
+  await fs.mkdir('data', { recursive: true }, (e) => { if (e) console.error(e) });
+  await fs.mkdir('data-dump', { recursive: true }, (e) => { if (e) console.error(e) });
 
-const dirCont = fs.readdirSync('data-dump');
-const files = dirCont.filter((filename) => filename.match(/.*\.xml/ig));
+  const dirCont = fs.readdirSync('data-dump');
+  const files = dirCont.filter((filename) => filename.match(/.*\.xml/ig));
 
-if (!files?.length) {
-  console.error('No XML file found, quiting..');
-  process.exit();
-}
-
-var xmlsplit = new XmlSplit(1, 'page')
-var inputStream = fs.createReadStream(`data-dump/${files[0]}`) // from somewhere
-const titles = new Set();
-inputStream.pipe(xmlsplit).on('data', function(data) {
-  let xmlDocument = data.toString();
-  const title = (xmlDocument.match(/<title>(.*)<\/title>/) || [,'undefined'])[1];
-
-  if (title.includes('/')) return;
-  if (
-    titles.has(title)
-    || title.includes('File:')
-    || title.includes('Talk:')
-    || title.includes('Forum:')
-    || title.includes('User:')
-    || title.includes('Message Wall:')
-    || title.includes('talk:')
-  ) return;
-
-  titles.add(title);
-  xmlDocument = `${xmlDocument.substring(0, xmlDocument.indexOf('<siteinfo>') - 3)}${xmlDocument.substring(xmlDocument.indexOf('</siteinfo>') + 12)}`
-
-  if (title.includes(':')) {
-    fs.promises.mkdir(`data/${title.replace(/:/g, '/').replace(/\/[^\/]+$/, '')}`, { recursive: true }).catch(console.error).then(() => {
-      fs.writeFileSync(`data/${title.replace(/:/g, '/')}.xml`, xmlDocument);
-    })
-  } else {
-    fs.writeFileSync(`data/${title.replace(/:/g, '/')}.xml`, xmlDocument);
+  if (!files?.length) {
+    console.error('No XML file found, quiting..');
+    process.exit();
   }
-})
+
+  console.log('Splitting wiki file..');
+
+  var xmlsplit = new XmlSplit(1, 'page')
+  var inputStream = fs.createReadStream(`data-dump/${files[0]}`) // from somewhere
+  const titles = new Set();
+  inputStream.pipe(xmlsplit).on('data', function(data) {
+    let xmlDocument = data.toString();
+    const title = (xmlDocument.match(/<title>(.*)<\/title>/) || [,'undefined'])[1];
+
+    if (title.includes('/')) return;
+    if (
+      titles.has(title)
+      || title.includes('File:')
+      || title.includes('Talk:')
+      || title.includes('Forum:')
+      || title.includes('User:')
+      || title.includes('Message Wall:')
+      || title.includes('talk:')
+    ) return;
+
+    titles.add(title);
+    xmlDocument = `${xmlDocument.substring(0, xmlDocument.indexOf('<siteinfo>') - 3)}${xmlDocument.substring(xmlDocument.indexOf('</siteinfo>') + 12)}`
+
+    if (title.includes(':')) {
+      fs.promises.mkdir(`data/${title.replace(/:/g, '/').replace(/\/[^\/]+$/, '')}`, { recursive: true }).catch(console.error).then(() => {
+        fs.writeFileSync(`data/${title.replace(/:/g, '/')}.xml`, xmlDocument);
+      })
+    } else {
+      fs.writeFileSync(`data/${title.replace(/:/g, '/')}.xml`, xmlDocument);
+    }
+  })
+
+})()
