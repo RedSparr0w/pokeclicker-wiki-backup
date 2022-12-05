6d434d70aa13ea263a5ae80fc43513938c60a804
Danial <admin@redsparr0w.com>
2022-12-05 19:02:21 +1300

    test

---
+++ index.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const XmlSplit = require('xmlsplit')
+
+const dirCont = fs.readdirSync('data-dump');
+const files = dirCont.filter((filename) => filename.match(/.*\.xml/ig));
+
+if (!files?.length) {
+  return console.error('No XML file found, quiting..');
+}
+
+var xmlsplit = new XmlSplit(1, 'page')
+var inputStream = fs.createReadStream(files[0]) // from somewhere
+fs.mkdir('data', { recursive: true }, (err) => {});
+const titles = new Set();
+inputStream.pipe(xmlsplit).on('data', function(data) {
+  let xmlDocument = data.toString();
+  const title = (xmlDocument.match(/<title>(.*)<\/title>/) || [,'undefined'])[1];
+
+  if (title.includes('/')) return;
+  if (
+    titles.has(title)
+    || title.includes('File:')
+    || title.includes('Talk:')
+    || title.includes('Forum:')
+    || title.includes('User:')
+    || title.includes('Message Wall:')
+    || title.includes('talk:')
+  ) return;
+
+  titles.add(title);
+  xmlDocument = `${xmlDocument.substring(0, xmlDocument.indexOf('<siteinfo>') - 3)}${xmlDocument.substring(xmlDocument.indexOf('</siteinfo>') + 12)}`
+
+  if (title.includes(':')) {
+    fs.promises.mkdir(`data/${title.replace(/:/g, '/').replace(/\/[^\/]+$/, '')}`, { recursive: true }).catch(console.error).then(() => {
+      fs.writeFileSync(`data/${title.replace(/:/g, '/')}.xml`, xmlDocument);
+    })
+  } else {
+    fs.writeFileSync(`data/${title.replace(/:/g, '/')}.xml`, xmlDocument);
+  }
+})
